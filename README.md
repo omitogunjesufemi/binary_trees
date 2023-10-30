8d20e82b0cc610c4b29b93babe84cb06b09b7c7c
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-10-30 12:20:20 +0100

    Set up project and added a function that creates a binary tree node

---
+++ README.md
@@ -0,0 +1,13 @@
+# 0x1D. C - Binary trees
+
+This project helped to understand:
+- Binary Tree
+- The difference between a binary tree and a Binary Search Tree
+- The possible gain in terms of time complexity compared to linked list
+- The depth, the height, the size of a binary tree
+- The different traversal methods to go through a binary tree
+- What a complete, full, perfect, balanced binary tree is
+
+*The project contains the following task:*
+TASK | DESCRIPTION
+--- | ---
---
+++ binary_trees.h
@@ -0,0 +1,39 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+typedef struct binary_tree_s bst_t;
+
+typedef struct binary_tree_s binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: a pointer to the parent node of the node to create
+ * @value: the value to put in the new node
+ * Created node does not have any child
+ * Return: pointer to the new node or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif /* _BINARY_TREES_H_ */
